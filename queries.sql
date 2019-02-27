1e264ce678c63f48dade1968acea945a2e13faa5
Marvin Diaz <marvindaviddiaz@gmail.com>
2019-02-27 12:20:15 -0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -10,23 +10,26 @@ ORDER BY (data_length + index_length) DESC;
 
 BEGIN
   DBMS_SCHEDULER.CREATE_JOB (
-      job_name        => 'LOG_DTE_DETAIL_PART_JOB',
+      job_name        => 'LOG_DTE_PART_JOB',
       job_type        => 'PLSQL_BLOCK',
       job_action      => 'DECLARE
                             partition_name  varchar2(5);
                             partition_until varchar2(10);
+                            tablespace_ddl  varchar2(255);
                             partition_ddl   varchar2(255);
                           BEGIN
                             SELECT TO_CHAR(ADD_MONTHS(current_date, 1), ''MON'') || TO_CHAR(ADD_MONTHS(current_date, 1), ''YY'') INTO partition_name FROM dual;
                             SELECT TO_CHAR(ADD_MONTHS(current_date, 2), ''YYYY-MM'') || ''-01'' INTO partition_until FROM dual;
-                            partition_ddl := ''ALTER TABLE LOG_DTE_DETAIL ADD PARTITION ''|| partition_name ||'' VALUES LESS THAN (TO_DATE ('''''' || partition_until || '''''', ''''YYYY-MM-DD'''')) TABLESPACE TBS_LOG_DTE_DETAIL'' || partition_name;
+                            tablespace_ddl := ''create tablespace TBS_LOG_DTE'' || partition_name || '' datafile ''''TBS_LOG_DTE'' || partition_name ||''.dbf'''' size 100m autoextend on next 100m maxsize unlimited;'';
+                            execute immediate tablespace_ddl;
+                            partition_ddl := ''ALTER TABLE LOG_DTE ADD PARTITION ''|| partition_name ||'' VALUES LESS THAN (TO_DATE ('''''' || partition_until || '''''', ''''YYYY-MM-DD'''')) TABLESPACE TBS_LOG_DTE'' || partition_name;
                             execute immediate partition_ddl;
                           END;',
       start_date      =>  NULL,
       repeat_interval => 'freq=monthly;bymonthday=10;byhour=8;byminute=0;bysecond=0;',
       end_date        =>  NULL,
       enabled         =>  TRUE,
-      comments        => 'Creación Partición LOG_DTE_DETAIL');
+      comments        => 'Creación Partición LOG_DTE');
 END;
 
 BEGIN
