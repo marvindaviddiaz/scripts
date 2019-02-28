73f3581159b4aacb7d1ff030b16baa72e53e64e0
Marvin Diaz <marvindaviddiaz@gmail.com>
2019-02-28 13:39:53 -0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -20,13 +20,13 @@ BEGIN
                           BEGIN
                             SELECT TO_CHAR(ADD_MONTHS(current_date, 1), ''MON'') || TO_CHAR(ADD_MONTHS(current_date, 1), ''YY'') INTO partition_name FROM dual;
                             SELECT TO_CHAR(ADD_MONTHS(current_date, 2), ''YYYY-MM'') || ''-01'' INTO partition_until FROM dual;
-                            tablespace_ddl := ''create tablespace TBS_LOG_DTE'' || partition_name || '' DATAFILE ''||CHR(39)||''+DATA''||CHR(39)||'' size 100m autoextend on next 100m maxsize unlimited;'';
+                            tablespace_ddl := ''create tablespace TBS_LOG_DTE'' || partition_name || '' DATAFILE ''||CHR(39)||''+DATA''||CHR(39)||'' size 100m reuse autoextend on next 100m extent management local segment space management auto'';
                             execute immediate tablespace_ddl;
                             partition_ddl := ''ALTER TABLE LOG_DTE ADD PARTITION ''|| partition_name ||'' VALUES LESS THAN (TO_DATE ('''''' || partition_until || '''''', ''''YYYY-MM-DD'''')) TABLESPACE TBS_LOG_DTE'' || partition_name;
                             execute immediate partition_ddl;
                           END;',
-      start_date      =>  NULL,
-      repeat_interval => 'freq=monthly;bymonthday=-1;byhour=8;byminute=0;bysecond=0;',
+			start_date      =>  '01-JAN-19 01.00.00 AM America/Guatemala',
+      repeat_interval => 'freq=monthly;bymonthday=-1;byhour=2;byminute=0;bysecond=0;',
       end_date        =>  NULL,
       enabled         =>  TRUE,
       comments        => 'Creación Partición LOG_DTE');
